7d163592e251a20db2bc12a42a7a1fe07ce97da0
Emerson Amorim <emerson_tecno@hotmail.com>
2023-05-03 11:32:31 -0300

    Lista-Item-App-ReactNative

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import { SwipeListView } from 'react-native-swipe-list-view';
 
 export default function App() {
+  const [compras, setCompras] = useState([]);
+  const [itemCompra, setItemCompra] = useState('');
+
+  const adicionarCompra = () => {
+    setCompras([...compras, { id: Date.now().toString(), nome: itemCompra }]);
+    setItemCompra('');
+  };
+
+  const removerCompra = (id) => {
+    setCompras(compras.filter((compra) => compra.id !== id));
+  };
+
+  const renderCompra = ({ item }) => (
+    <View style={styles.item}>
+      <Text style={styles.itemText}>{item.nome}</Text>
+    </View>
+  );
+
+  const renderHiddenItem = ({ item }) => (
+    <View style={styles.hiddenItem}>
+      <TouchableOpacity style={styles.deleteButton} onPress={() => removerCompra(item.id)}>
+        <Text style={styles.deleteButtonText}>Delete</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Lista de Compras</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite o item de compra"
+        value={itemCompra}
+        onChangeText={setItemCompra}
+      />
+      <Button title="Adicionar item" onPress={adicionarCompra} />
+      <SwipeListView
+        style={styles.list}
+        data={compras}
+        keyExtractor={(item) => item.id}
+        renderItem={renderCompra}
+        renderHiddenItem={renderHiddenItem}
+        rightOpenValue={-75}
+      />
     </View>
   );
 }
@@ -17,4 +58,57 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: '90%',
+  },
+  list: {
+    width: '100%',
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    width: '100%',
+    borderColor: 'gray',
+    borderBottomWidth: 1,
+    backgroundColor: 'white',
+  },
+  itemText: {
+    flex: 1,
+  },
+  hiddenItem: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    padding: 10,
+    width: '100%',
+    borderColor: 'gray',
+    borderBottomWidth: 1,
+    backgroundColor: 'white',
+  },
+  deleteButton: {
+    width: 75,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red',
+    opacity: 0,
+  },
+  deleteButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
 });
+
+
+
---
+++ README.md
@@ -0,0 +1,25 @@
+## Lista de Compras App
+Codificação de um aplicativo React Native simples que permite ao usuário criar uma lista de compras e marcar itens como concluídos. O aplicativo usa o pacote "react-native-swipe-list-view" para criar uma lista de compras deslizável, onde o usuário pode excluir itens deslizando-os para a esquerda.
+
+- Como Usar
+Para usar este aplicativo, siga estas etapas:
+
+Clone ou baixe este repositório em seu computador.
+Instale as dependências usando o comando npm install.
+Inicie o aplicativo usando o comando npm start.
+Execute o aplicativo em um emulador ou dispositivo físico.
+
+- Funcionalidades
+O aplicativo possui as seguintes funcionalidades:
+
+Adicionar um item à lista de compras
+Excluir um item da lista de compras
+Tecnologias Utilizadas
+Este aplicativo foi desenvolvido usando as seguintes tecnologias:
+
+- React Native
+- JavaScript
+- Bibliotecas Externas
+O aplicativo usa as seguintes bibliotecas externas:
+
+react-native-swipe-list-view: Para criar uma lista de compras deslizável.
